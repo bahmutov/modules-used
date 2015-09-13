1b9bc15cd0aa51832de8a94eb7b07f2c64575648
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-13 17:55:57 -0400

    added readme

--- modules-used.js
+++ modules-used.js
@@ -1,22 +1,35 @@
-var log = require('debug')('used');
-var pkg = require('./package.json');
-var join = require('path').join;
-var read = require('fs').readFileSync;
+function modulesUsed() {
+  var log = require('debug')('used');
+  var pkg = require('./package.json');
+  var join = require('path').join;
+  var read = require('fs').readFileSync;
 
-function getProdDependenciesList(pkg) {
-  return pkg && pkg.dependencies ? Object.keys(pkg.dependencies) : [];
+  function getProdDependenciesList(pkg) {
+    return pkg && pkg.dependencies ? Object.keys(pkg.dependencies) : [];
+  }
+
+  function dependencyDescription(name) {
+    var filename = join('.', 'node_modules', name, 'package.json');
+    var pkg = JSON.parse(read(filename));
+    return {
+      name: pkg.name,
+      description: pkg.description
+    };
+  }
+
+  function toMarkdown(pkgInfo) {
+    return '* ' + pkgInfo.name + ' - ' + pkgInfo.description;
+  }
+
+  var prodDependencies = getProdDependenciesList(pkg);
+  var described = prodDependencies.map(dependencyDescription);
+  var markdown = described.map(toMarkdown).join('\n');
+  log(markdown);
+  return markdown;
 }
 
-function dependencyDescription(name) {
-  var filename = join('.', 'node_modules', name, 'package.json');
-  var pkg = JSON.parse(read(filename));
-  return {
-    name: pkg.name,
-    description: pkg.description
-  };
+module.exports = modulesUsed;
+
+if (!module.parent) {
+  modulesUsed();
 }
-
-var prodDependencies = getProdDependenciesList(pkg);
-var described = prodDependencies.map(dependencyDescription);
-log(described);
-
