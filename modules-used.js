c256f35bc141d0070e719ae6a6eb7f11fc69ed1a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-13 18:06:46 -0400

    using cwd

--- modules-used.js
+++ modules-used.js
@@ -1,15 +1,18 @@
 function modulesUsed() {
   var log = require('debug')('used');
   var pkg = require('./package.json');
+  log('modules used in', pkg.name);
+
   var join = require('path').join;
   var read = require('fs').readFileSync;
+  var cwd = process.cwd();
 
   function getProdDependenciesList(pkg) {
     return pkg && pkg.dependencies ? Object.keys(pkg.dependencies) : [];
   }
 
   function dependencyDescription(name) {
-    var filename = join('.', 'node_modules', name, 'package.json');
+    var filename = join(cwd, 'node_modules', name, 'package.json');
     var pkg = JSON.parse(read(filename));
     return {
       name: pkg.name,
